dfa61184d019aa4ea9647a31db1b2dd2512ae89b
kuldipsinh22 <131747159+kuldipsinh22@users.noreply.github.com>
2024-03-10 17:44:48 +0530

    error

--- back/controllers/personalproperty.js
+++ back/controllers/personalproperty.js
@@ -1,7 +1,16 @@
 import { db } from "../db.js";
 
-export const getproperty = (req, res) => {
+export const getProperty = (req, res) => {
   const query = "select * from property where dealer_id=?";
+
+  db.query(query, [req.params.id], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data[0]);
+  });
+};
+
+export const getproperties = (req, res) => {
+  const query = "select * from property";
 
   db.query(query, (err, data) => {
     if (err) return res.json(err);
--- back/models/r_pProperty.js
+++ back/models/r_pProperty.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getproperty } from "../controllers/personalproperty.js";
+import { getProperty, getproperties } from "../controllers/personalproperty.js";
 
 const router7 = express.Router();
 
-router7.get("/", getproperty);
+router7.get("/:id", getProperty);
+router7.get("/", getproperties);
 export default router7;
