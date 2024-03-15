bb0a81fbf29698269f02ede8deb14bb88a531df3
kuldipsinh22 <131747159+kuldipsinh22@users.noreply.github.com>
2024-03-16 03:29:09 +0530

    16/3/24

--- main/src/components/Profile.jsx
+++ main/src/components/Profile.jsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Profile() {
+  const [username, setusername] = useState("");
+  const [img, setimg] = useState("");
+  const [contact, setcontact] = useState("");
+  const [user_email, setuser_email] = useState("");
+  const [password, setpassword] = useState("");
+  const [city, setcity] = useState("");
+  const [state, setstate] = useState("");
+
+  // const [id, setId] = useState(sessionStorage.getItem("user"));
+
   return (
-    <div class="g-sidenav-show bg-gray-100">
+    <div class="g-sidenav-show bg-gray-100 mt-5" style={{ height: "auto" }}>
       <div
-        class="position-absolute w-100 min-height-300 top-0"
-        style={{
-          backgroundImage:
-            "url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/profile-layout-header.jpg')",
-          backgroundPositionY: "50%",
-        }}
-      >
-        <span class="mask bg-primary opacity-6"></span>
-      </div>
-      <div
-        class="main-content position-relative max-height-vh-100 h-100"
+        class="main-content position-relative max-height-vh-100 "
         style={{ marginLeft: "17%" }}
       >
-        <div class="card shadow-lg mx-4 card-profile-bottom">
+        <div class="card shadow-lg mx-4 card-profile-top">
           <div class="card-body p-3">
             <div class="row gx-4">
               <div class="col-auto">
@@ -40,14 +40,11 @@ export default function Profile() {
         </div>
         <div class="container-fluid py-4">
           <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-12">
               <div class="card">
                 <div class="card-header pb-0">
                   <div class="d-flex align-items-center">
                     <p class="mb-0">Edit Profile</p>
-                    <button class="btn btn-primary btn-sm ms-auto">
-                      Settings
-                    </button>
                   </div>
                 </div>
                 <div class="card-body">
@@ -61,26 +58,7 @@ export default function Profile() {
                         >
                           Username
                         </label>
-                        <input
-                          class="form-control"
-                          type="text"
-                          value="lucky.jesse"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-group">
-                        <label
-                          for="example-text-input"
-                          class="form-control-label"
-                        >
-                          Email address
-                        </label>
-                        <input
-                          class="form-control"
-                          type="email"
-                          value="jesse@example.com"
-                        />
+                        <input class="form-control" type="text" />
                       </div>
                     </div>
                     <div class="col-md-6">
@@ -91,7 +69,7 @@ export default function Profile() {
                         >
                           First name
                         </label>
-                        <input class="form-control" type="text" value="Jesse" />
+                        <input class="form-control" type="text" />
                       </div>
                     </div>
                     <div class="col-md-6">
@@ -100,31 +78,28 @@ export default function Profile() {
                           for="example-text-input"
                           class="form-control-label"
                         >
-                          Last name
+                          Email address
                         </label>
-                        <input class="form-control" type="text" value="Lucky" />
+                        <input class="form-control" type="email" />
                       </div>
                     </div>
-                  </div>
-                  <hr class="horizontal dark" />
-                  <p class="text-uppercase text-sm">Contact Information</p>
-                  <div class="row">
-                    <div class="col-md-12">
+
+                    <div class="col-md-6">
                       <div class="form-group">
                         <label
                           for="example-text-input"
                           class="form-control-label"
                         >
-                          Address
+                          Contact
                         </label>
-                        <input
-                          class="form-control"
-                          type="text"
-                          value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                        />
+                        <input class="form-control" type="text" />
                       </div>
                     </div>
-                    <div class="col-md-4">
+                  </div>
+                  <hr class="horizontal dark" />
+                  <p class="text-uppercase text-sm">img Information</p>
+                  <div class="row">
+                    <div class="col-md-6">
                       <div class="form-group">
                         <label
                           for="example-text-input"
@@ -132,14 +107,23 @@ export default function Profile() {
                         >
                           City
                         </label>
-                        <input
-                          class="form-control"
-                          type="text"
-                          value="New York"
-                        />
+                        <input class="form-control" type="file" />
                       </div>
                     </div>
-                    <div class="col-md-4">
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label
+                          for="example-text-input"
+                          class="form-control-label"
+                        >
+                          City
+                        </label>
+                        <input class="form-control" type="text" />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
                       <div class="form-group">
                         <label
                           for="example-text-input"
@@ -147,190 +131,19 @@ export default function Profile() {
                         >
                           Country
                         </label>
-                        <input
-                          class="form-control"
-                          type="text"
-                          value="United States"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-md-4">
-                      <div class="form-group">
-                        <label
-                          for="example-text-input"
-                          class="form-control-label"
-                        >
-                          Postal code
-                        </label>
-                        <input
-                          class="form-control"
-                          type="text"
-                          value="437300"
-                        />
+                        <input class="form-control" type="text" />
                       </div>
                     </div>
                   </div>
                   <hr class="horizontal dark" />
-                  <p class="text-uppercase text-sm">About me</p>
-                  <div class="row">
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label
-                          for="example-text-input"
-                          class="form-control-label"
-                        >
-                          About me
-                        </label>
-                        <input
-                          class="form-control"
-                          type="text"
-                          value="A beautiful Dashboard for Bootstrap 5. It is Free and Open Source."
-                        />
-                      </div>
-                    </div>
-                  </div>
+                  <button class="btn btn-success btn-sm ms-auto">
+                    Save changes
+                  </button>
+                  <button class="btn btn-danger btn-sm ms-5">
+                    Delete account
+                  </button>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="fixed-plugin">
-        <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-          <i class="fa fa-cog py-2"> </i>
-        </a>
-        <div class="card shadow-lg">
-          <div class="card-header pb-0 pt-3 ">
-            <div class="float-start">
-              <h5 class="mt-3 mb-0">Argon Configurator</h5>
-              <p>See our dashboard options.</p>
-            </div>
-            <div class="float-end mt-4">
-              <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-                <i class="fa fa-close"></i>
-              </button>
-            </div>
-            {/* <!-- End Toggle Button --> */}
-          </div>
-          <hr class="horizontal dark my-1" />
-          <div class="card-body pt-sm-3 pt-0 overflow-auto">
-            {/* <!-- Sidebar Backgrounds --> */}
-            <div>
-              <h6 class="mb-0">Sidebar Colors</h6>
-            </div>
-            <a
-              href="javascript:void(0)"
-              class="switch-trigger background-color"
-            >
-              <div class="badge-colors my-2 text-start">
-                <span
-                  class="badge filter bg-gradient-primary active"
-                  data-color="primary"
-                  onclick="sidebarColor(this)"
-                ></span>
-                <span
-                  class="badge filter bg-gradient-dark"
-                  data-color="dark"
-                  onclick="sidebarColor(this)"
-                ></span>
-                <span
-                  class="badge filter bg-gradient-info"
-                  data-color="info"
-                  onclick="sidebarColor(this)"
-                ></span>
-                <span
-                  class="badge filter bg-gradient-success"
-                  data-color="success"
-                  onclick="sidebarColor(this)"
-                ></span>
-                <span
-                  class="badge filter bg-gradient-warning"
-                  data-color="warning"
-                  onclick="sidebarColor(this)"
-                ></span>
-                <span
-                  class="badge filter bg-gradient-danger"
-                  data-color="danger"
-                  onclick="sidebarColor(this)"
-                ></span>
-              </div>
-            </a>
-            {/* <!-- Sidenav Type --> */}
-            <div class="mt-3">
-              <h6 class="mb-0">Sidenav Type</h6>
-              <p class="text-sm">Choose between 2 different sidenav types.</p>
-            </div>
-            <div class="d-flex">
-              <button
-                class="btn bg-gradient-primary w-100 px-3 mb-2 active me-2"
-                data-class="bg-white"
-                onclick="sidebarType(this)"
-              >
-                White
-              </button>
-              <button
-                class="btn bg-gradient-primary w-100 px-3 mb-2"
-                data-class="bg-default"
-                onclick="sidebarType(this)"
-              >
-                Dark
-              </button>
-            </div>
-            <p class="text-sm d-xl-none d-block mt-2">
-              You can change the sidenav type just on desktop view.
-            </p>
-            {/* <!-- Navbar Fixed --> */}
-            <hr class="horizontal dark my-sm-4" />
-            <div class="mt-2 mb-5 d-flex">
-              <h6 class="mb-0">Light / Dark</h6>
-              <div class="form-check form-switch ps-0 ms-auto my-auto">
-                <input
-                  class="form-check-input mt-1 ms-auto"
-                  type="checkbox"
-                  id="dark-version"
-                  onclick="darkMode(this)"
-                />
-              </div>
-            </div>
-            <a
-              class="btn bg-gradient-dark w-100"
-              href="https://www.creative-tim.com/product/argon-dashboard"
-            >
-              Free Download
-            </a>
-            <a
-              class="btn btn-outline-dark w-100"
-              href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard"
-            >
-              View documentation
-            </a>
-            <div class="w-100 text-center">
-              <a
-                class="github-button"
-                href="https://github.com/creativetimofficial/argon-dashboard"
-                data-icon="octicon-star"
-                data-size="large"
-                data-show-count="true"
-                aria-label="Star creativetimofficial/argon-dashboard on GitHub"
-              >
-                Star
-              </a>
-              <h6 class="mt-3">Thank you for sharing!</h6>
-              <a
-                href="https://twitter.com/intent/tweet?text=Check%20Argon%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fargon-dashboard"
-                class="btn btn-dark mb-0 me-2"
-                target="_blank"
-              >
-                <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-              </a>
-              <a
-                href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/argon-dashboard"
-                class="btn btn-dark mb-0 me-2"
-                target="_blank"
-              >
-                <i class="fab fa-facebook-square me-1" aria-hidden="true"></i>{" "}
-                Share
-              </a>
             </div>
           </div>
         </div>
